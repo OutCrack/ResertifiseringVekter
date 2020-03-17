216877750740b755711a7c478bfc3baa2b920527
Andreas Stenseng Bjørnrud <andreas@bjornrud.com>
2020-03-17 10:15:19 +0100

    Update examFinishedScreen.js

--- screens/examFinishedScreen.js
+++ screens/examFinishedScreen.js
@@ -3,11 +3,32 @@ import { View, Text, Button, StyleSheet } from "react-native";
 
 const examFinishedScreen = props => {
   return(
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 30 }}>This is a modal!</Text>
+    <View style={styles.container}>
+      <Text style={styles.font}>This is a modal!</Text>
       <Button onPress={() => navigation.goBack()} title="Dismiss" />
+			<View style={headerContainer}>
+			<Text style={styles.headerTxt}>Du klarte {props.correct} av {props.questions}</Text>
+			</View>
+			<View style={questionContainer}>
+			</View>
     </View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center"
+	},
+	headerContainer: {
+	
+	},
+	questionContainer: {
+		
+	};
+	font: {
+		fontSize: 30
+	}
+});
 
 export default examFinishedScreen;
